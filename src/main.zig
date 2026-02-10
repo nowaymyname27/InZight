5ba46f1e6ec850dc47993906723e4c96433ad9be
Jose Ramirez <galock@bu.edu>
2026-02-10 18:28:12 -0500

    Created my own simple allocator in Zig

--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,47 @@
 const std = @import("std");
 const InZight = @import("InZight");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var spy = try SpyAllocator.init(allocator, 10);
-    defer spy.deinit();
-
-    for (spy.slice, 0..) |*item, i| {
-        item.* = @intCast(i * 10);
-    }
-
-    std.debug.print("Memory contents: {any}\n", .{spy.slice});
+pub fn main() void {
+    std.log.info("Hello world", .{});
 }
 
 pub const SpyAllocator = struct {
-    slice: []i32,
-    allocator: std.mem.Allocator,
+    // State: parent allocator to do the actual work
+    parent_allocator: std.mem.Allocator,
 
     const Self = @This();
 
-    pub fn init(allocator: std.mem.Allocator, size: usize) !Self {
-        const slice = try allocator.alloc(i32, size);
+    pub fn init(parent_allocator: std.mem.Allocator) !Self {
         return Self{
-            .slice = slice,
-            .allocator = allocator,
+            .parent_allocator = parent_allocator,
         };
     }
 
-    pub fn deinit(self: Self) void {
-        self.allocator.free(self.slice);
+    pub fn allocator(self: *Self) std.mem.Allocator {
+        return .{
+            .ptr = self,
+
+            .vtable = &.{
+                .alloc = alloc,
+                .resize = resize,
+                .free = free,
+            },
+        };
+    }
+
+    // The Functions: Implement the logic
+    fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
+        const self: *SpyAllocator = @ptrCast(@alignCast(ctx));
+        return self.parent_allocator.rawAlloc(len, ptr_align, ret_addr);
+    }
+
+    fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+        const self: *SpyAllocator = @ptrCast(@alignCast(ctx));
+        return self.parent_allocator.rawResize(buf, buf_align, new_len, ret_addr);
+    }
+
+    fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
+        const self: *SpyAllocator = @ptrCast(@alignCast(ctx));
+        return self.parent_allocator.rawFree(buf, buf_align, ret_addr);
     }
 };
